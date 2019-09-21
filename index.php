123eaf6c719bbe7ee39be54f53c692c772c113f8
hakhoa0901 <hakhoa7s1@gmail.com>
2019-09-21 11:14:24 +0700

    test

--- index.php
+++ index.php
@@ -1,14 +1,42 @@
-body{
-	background-color: #FFFFCC
-}
-h2{
-	text-decoration: underline;
-}
-#navigation{
-	position: absolute;
-	left: 25%;
-	text-align: center;
-}
-#ablesectionOne{
-	
-}
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+<button onclick="Nhapa()">Nhap a</button>
+<button onclick="Nhapb()">Nhap b</button>
+<button onclick="Tong()">Tong</button>
+<button onclick="Hieu()">Hieu</button>
+<button onclick="Tich()">Tich</button>
+<button onclick="Thuong()">Thuong</button>
+<script type="text/javascript">
+	function Nhapa(message) {
+		a=prompt('Nhap a')
+		a=parseFloat(a)
+	}
+	function Nhapb(){
+		b=prompt('Nhap b')
+		b=parseFloat(b)
+	}
+	function Tong(){
+		Tong = a+b
+		alert(Tong)
+	}
+	function Hieu(){
+		Hieu = a-b
+		alert(Hieu)
+	}
+	function Tich(){
+		Tich = a*b
+		alert(Tich)
+	}
+	function Thuong(){
+		Thuong = a/b
+		alert(Thuong)
+	}
+</script>
+
+</body>
+</html>
